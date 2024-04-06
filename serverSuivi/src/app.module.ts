ef9c0023f26ea015cbd4912632d83d276269ffb4
mehdirayan <105606411+mehdirayan@users.noreply.github.com>
2024-04-06 02:56:32 +0200

    Add files via upload

---
+++ serverSuivi/src/app.module.ts
@@ -0,0 +1,34 @@
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { ActivitéModule } from './activité/activité.module';
+import { UsersModule } from './users/users.module';
+import { ClientModule } from './client/client.module';
+import { ÉquipementModule } from './équipement/équipement.module';
+import { DtModule } from './dt/dt.module';
+import { LivraisonModule } from './livraison/livraison.module';
+import { EtatDtMiddleware } from './dt/middlewares/etatDt.middleware';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    ActivitéModule,
+    UsersModule,
+    ClientModule,
+    ÉquipementModule,
+    DtModule,
+    LivraisonModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(EtatDtMiddleware)
+      .forRoutes({ path: 'dts/*', method: RequestMethod.PUT });
+  }
+}
--- serverSuivi/src/main.ts
+++ serverSuivi/src/main.ts
@@ -1,1 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './filters/exception.filter';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true, origin: '*' });
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter(new Logger()));
+  await app.listen(
+    process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    process.env.SERVER,
+    () => {
+      new Logger().log(
+        'Listen at ' + process.env.SERVER + ':' + process.env.PORT,
+        'APP',
+      );
+    },
+  );
+}
+bootstrap();
